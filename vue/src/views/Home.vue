0bef23ed7e466d7404cd9062efadc70843cb0319
Kaoru Chisen <cordx56@cordx.net>
2019-08-02 20:07:34 +0900

    update messages

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,22 +1,22 @@
 <template lang="pug">
   b-container.home
     h1.my-3 Tweet generator
-    p 人のツイートを学習して本人っぽい文章を生成するが……
+    p ツイートを学習して本人っぽい文章を生成するよ！
     p マルコフ連鎖によるツイート自動生成プログラム
     p This program uses <a href="https://github.com/jsvine/markovify" target="_blank">jsvine/markovify</a>.
-    p Detail: <a href="https://github.com/cordx56/tweet-generator" target="_blank">cordx56/tweet-generator</a>.
+    p Detail/Bug report: <a href="https://github.com/cordx56/tweet-generator" target="_blank">cordx56/tweet-generator</a>.
 
     b-card(v-if="sentence",header="生成結果",border-variant="info",header-bg-variant="info",header-text-variant="white",align="center")
       b-card-text {{ sentence }}
       b-card-text
-        b-button.btn-sm(variant="primary",v-bind:href="tweetLink",target="_blank") <font-awesome-icon :icon="['fab', 'twitter']"></font-awesome-icon> ツイート
+        b-button(variant="primary",size="sm",v-bind:href="tweetLink",target="_blank") <font-awesome-icon :icon="['fab', 'twitter']"></font-awesome-icon> ツイート
     b-card(v-if="errorMsg",header="エラー",border-variant="danger",header-bg-variant="danger",header-text-variant="white",align="center")
       b-card-text {{ errorMsg }}
     b-card(v-if="loadingMsg",header="読み込み中",border-variant="secondary",header-bg-variant="secondary",header-text-variant="white",align="center")
       b-card-text {{ loadingMsg }}
     b-card(v-if="$route.query.success",header="学習成功",border-variant="success",header-bg-variant="success",header-text-variant="white",align="center")
       b-card-text {{ $route.query.success }}
-      b-card-text いっぱい遊んでいってね、楽しんでもらえたらうれしいな。ほめてもらえるともっとうれしいかも……💕
+      b-card-text いっぱい遊んでいってね。楽しんでもらえたらうれしいな。ほめてもらえるともっとうれしいかも……
     b-card(v-if="$route.query.error",header="学習失敗",border-variant="danger",header-bg-variant="danger",header-text-variant="white",align="center")
       b-card-text {{ $route.query.error }}
 
