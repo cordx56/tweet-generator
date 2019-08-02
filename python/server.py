c5c5ab497b63a97c08f80bc93795c4ceea0c43d6
Kaoru Chisen <cordx56@cordx.net>
2019-08-02 19:41:56 +0900

    parse & check startWith word

--- python/server.py
+++ python/server.py
@@ -5,12 +5,15 @@ import datetime
 import markovify
 from flask import Flask, request, redirect, abort, jsonify
 import urllib.parse
+import MeCab
 import exportModel
 from twitterTools import TwitterTools
 
 app = Flask(__name__)
 
 twitterKeys = { "CK": os.environ.get("TWITTER_API_CONKEY"), "CS": os.environ.get("TWITTER_API_CONSEC") }
+
+mec = MeCab.Tagger("-d /usr/lib/mecab/dic/mecab-ipadic-neologd -O wakati")
 
 
 # Twitter related pages
@@ -64,8 +67,12 @@ def textGen(screenName = None):
         with open("./chainfiles/" + screenName + ".json") as f:
             textModel = markovify.Text.from_json(f.read())
         reqJson = request.json
-        if reqJson is not None and reqJson["startWith"]:
-            sentence = textModel.make_sentence_with_start(reqJson["startWith"], tries = 100)
+        if reqJson is not None and reqJson["startWith"] and 0 < len(reqJson["startWith"]):
+            startWithStr = mec.parse(reqJson["startWith"])
+            try:
+                sentence = textModel.make_sentence_with_start(startWithStr, tries = 100)
+            except KeyError:
+                return jsonify({ "status": False, "message": "生成失敗。該当開始語が存在しません。" }), 500
         elif reqJson is not None and str(reqJson["length"]).isdecimal():
             sentence = textModel.make_short_sentence(int(reqJson["length"]), tries = 100)
         else:
