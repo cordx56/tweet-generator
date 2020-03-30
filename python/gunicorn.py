2d22a6dfe39e565ef4cf24f4f81e9780e7a5f88f
Kaoru Chisen <cordx56@cordx.net>
2020-03-30 18:55:22 +0900

    gunicorn log x-forwarded-for

--- python/gunicorn.py
+++ python/gunicorn.py
@@ -1,5 +1,6 @@
 bind = '0.0.0.0:5000'
 accesslog = './log/gunicorn/access.log'
+access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 errorlog = './log/gunicorn/access.log'
 capture_output = True
 workers = 4
