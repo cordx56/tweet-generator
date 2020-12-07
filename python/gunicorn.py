826c0f04edfe52f2a00474f3c181a0abd2bc882c
Kaoru Chisen <cordx56@cordx.net>
2020-12-07 21:23:41 +0900

    cleanup

--- python/gunicorn.py
+++
@@ -1,8 +0,0 @@
-bind = '0.0.0.0:5000'
-accesslog = './log/gunicorn/access.log'
-access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-errorlog = './log/gunicorn/access.log'
-capture_output = True
-workers = 4
-max_requests = 4000
-max_requests_jitter = 400
