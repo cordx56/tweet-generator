b8e72159cb149c639956054e1826ca5638ac9742
Kaoru Chisen <cordx56@cordx.net>
2020-12-08 18:38:00 +0900

    update title

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'tweet_generator',
+    title: 'Tweet generator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
