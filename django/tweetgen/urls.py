cec931fa7acfbc67ae11640e48a4278196406cca
Kaoru Chisen <cordx56@cordx.net>
2021-09-27 00:21:45 +0900

    Revert "new data structure for markov chain"
    This reverts commit 8e45ce515df0d6ec410a5a88a75893bc484dfd12.

--- django/tweetgen/urls.py
+++ django/tweetgen/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('api/v1/tweetgen/authAndGen/', views.AuthAndGenAPIView.as_view()),
     path('api/v1/tweetgen/authAndDel/', views.AuthAndDelAPIView.as_view()),
 
-    path('api/v1/tweetgen/genText/', views.GenTextAPIView.as_view()),
-    path('api/v1/tweetgen/genText/<screen_names>', views.GenTextAPIView.as_view()),
+    path('api/v1/tweetgen/genText/<screen_name>', views.GenTextAPIView.as_view()),
 
     # Twitter card image
     path('api/v1/tweetgen/genImage/', views.GenImageAPIView.as_view()),
